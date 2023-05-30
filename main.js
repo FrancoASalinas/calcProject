6084250e144bef1fdc8d88f705a46eff9f04dc4f
FrancoSMiño <frankow222@gmail.com>
2023-05-29 23:56:18 -0300

    1.0

---
+++ main.js
@@ -0,0 +1,20 @@
+const number = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
+const erase = document.querySelector('.erase');
+const reset = document.querySelector('.reset');
+const displayActual = document.querySelector('.valor-actual');
+const displayAnterior = document.querySelector('.valor-anterior')
+
+const display = new Display(displayAnterior, displayActual);
+
+number.forEach(boton => boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML)));
+
+operator.forEach(boton => boton.addEventListener('click', ()=> display.computar(boton.value)));
+
+erase.addEventListener('click',()=> display.borrar());
+
+reset.addEventListener('click',()=> display.borrarTodo());
+
+
+
+
